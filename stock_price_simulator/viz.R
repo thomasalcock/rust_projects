0ee961fe0db190c491411639e1c10d24c95cdbaa
thomasalcock <thomas.alcock@statworx.com>
2023-10-29 11:08:13 +0100

    add .gitignore and update viz script

--- stock_price_simulator/viz.R
+++ stock_price_simulator/viz.R
@@ -1,7 +1,6 @@
 library(ggplot2)
-df <- read.csv("outputs/output.csv", header=T)
+df <- read.csv("outputs/simulation.csv", header=T)
 df$path_index <- as.character(df$path_index)
-dim(df)
 
 ggplot(df, aes(x = time_index, y = price, col = path_index)) + 
     geom_line() + theme_minimal() + 
