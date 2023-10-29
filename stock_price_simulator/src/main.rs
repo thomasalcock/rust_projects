582a2eceae56bd4c0e65a7d7da59b157894e0a50
thomasalcock <thomas.alcock@statworx.com>
2023-10-29 10:54:28 +0100

    use clap to build cli tool

--- stock_price_simulator/src/main.rs
+++ stock_price_simulator/src/main.rs
@@ -1,24 +1,53 @@
+extern crate clap;
 extern crate csv;
 extern crate ndarray;
 extern crate ndarray_rand;
 
 use csv::Writer;
 use std::error::Error;
+use std::usize;
 
 use ndarray::{Array, Array2};
 use ndarray_rand::rand_distr::Normal;
 use ndarray_rand::RandomExt;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let output_path: &str = "outputs/output.csv";
+use clap::Parser;
 
-    // Set parameters
-    let n_paths = 50;
-    let n_steps = 1000;
-    let maturity = 1.0;
-    let r = 0.05;
-    let sigma = 0.4;
-    let s_0 = 100.0;
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct SimulationArgs {
+    #[arg(long, default_value_t = 25)]
+    n_paths: usize,
+
+    #[arg(long, default_value_t = 100)]
+    n_steps: usize,
+
+    #[arg(long, default_value_t = 1.0)]
+    maturity: f64,
+
+    #[arg(long, default_value_t = 0.04)]
+    r: f64,
+
+    #[arg(long, default_value_t = 0.5)]
+    sigma: f64,
+
+    #[arg(long, default_value_t = 100.0)]
+    initial_stock_price: f64,
+
+    #[arg(long)]
+    output_path: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Get and check command line args
+    let args = SimulationArgs::parse();
+    let n_steps = args.n_steps;
+    let n_paths = args.n_paths;
+    let maturity = args.maturity;
+    let r = args.r;
+    let sigma = args.sigma;
+    let initial_stock_price = args.initial_stock_price;
+    let output_path = args.output_path;
 
     // Generate random numbers
     let z_values = Array::random((n_steps, n_paths), Normal::new(r, sigma).unwrap());
@@ -30,7 +59,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Vector to store paths
     let mut paths: Array2<f64> = Array2::<f64>::zeros((n_steps + 1, n_paths));
-    paths.fill(f64::ln(s_0));
+    paths.fill(f64::ln(initial_stock_price));
 
     let mut time_index = Array::range(0., n_steps as f64, 1.);
     time_index = time_index.map(|i| i as &f64 * dt);
@@ -48,18 +77,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     // Write results to CSV file
+    println!("Writing file to {}", output_path);
     let mut writer = Writer::from_path(output_path)?;
     writer.write_record(&["path_index", "time_index", "price"])?;
 
     for i in 0..n_steps {
         for j in 0..n_paths {
-            let j_str: String = j.to_string();
-            let ti_str = time_index[i].to_string();
-            let p_str = paths[[i, j]].to_string();
-            let _ = writer.write_record(&[j_str, ti_str, p_str]);
+            let _ = writer.write_record(&[
+                j.to_string(),
+                time_index[i].to_string(),
+                paths[[i, j]].to_string(),
+            ]);
         }
     }
-
-    println!("Wrote file to {}", output_path);
+    println!("Done.");
     Ok(())
 }
