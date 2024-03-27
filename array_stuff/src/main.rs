7e45fa6fa6344fd77040fc11a7ad9194ca394dfd
thomasalcock <thomas.alcock@statworx.com>
2024-03-27 16:02:05 +0100

    add test for multiplcation

--- array_stuff/src/main.rs
+++ array_stuff/src/main.rs
@@ -1,12 +1,6 @@
 use ::rand::Rng;
 use rand::rngs::ThreadRng;
 
-// #[derive(Debug)]
-// struct Vector {
-//     n: usize,
-//     data: Vec<f32>,
-// }
-
 #[derive(Debug)]
 struct Matrix {
     nrows: usize,
@@ -14,34 +8,14 @@ struct Matrix {
     data: Vec<f32>,
 }
 
-// impl Vector {
-
-//     fn random(n: usize, min: f32, max: f32) -> Vector {
-//         let mut rng: ThreadRng = rand::thread_rng();
-//         let mut result: Vector = Vector{n: n, data: Vec::new()};
-//         result.data.reserve(n);
-//         for i in 0..n {
-//             result.data.push(rng.gen_range(min..max));
-//         }
-//         return result;
-//     }
-
-//     fn dot(&self, v: &Vector) {
-
-//     }
-// }
-
-
 impl Matrix {
-
     fn random(nrows: usize, ncols: usize, min: f32, max: f32) -> Matrix {
         let mut rng: ThreadRng = rand::thread_rng();
         let mut matrix: Matrix = Matrix {
             nrows: nrows,
             ncols: ncols,
-            data: Vec::new(),
+            data: vec![0.0; nrows * ncols],
         };
-        matrix.data.reserve(nrows * ncols);
         for _ in 0..(nrows * ncols) {
             matrix.data.push(rng.gen_range(min..max));
         }
@@ -55,7 +29,7 @@ impl Matrix {
             println!("{:?}", &self.data[start..(start + self.ncols)]);
         }
     }
-    
+
     fn multiply(&self, m: &Matrix) -> Matrix {
         if self.ncols != m.nrows {
             panic!("k != n");
@@ -73,9 +47,9 @@ impl Matrix {
                 for k in 0..self.ncols {
                     a = self.data[i * self.ncols + k];
                     b = m.data[k * m.ncols + j];
-                    result.data[i * result.ncols + j] +=  a * b;
+                    result.data[i * result.ncols + j] += a * b;
                 }
-            } 
+            }
         }
 
         return result;
@@ -83,12 +57,47 @@ impl Matrix {
 }
 
 fn main() {
-
-    let a: Matrix = Matrix{ncols: 2, nrows: 2, data: vec![1.0, 2., 3., 4.]};
-    let b: Matrix = Matrix{ncols: 2, nrows: 2, data: vec![5., 6., 7., 8.]};
+    let a: Matrix = Matrix {
+        ncols: 2,
+        nrows: 2,
+        data: vec![1.0, 2., 3., 4.],
+    };
+    let b: Matrix = Matrix {
+        ncols: 2,
+        nrows: 2,
+        data: vec![5., 6., 7., 8.],
+    };
     let c: Matrix = a.multiply(&b);
-    
+
     a.print_matrix();
     b.print_matrix();
     c.print_matrix();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn matrix_multiplication_works() {
+        let a: Matrix = Matrix {
+            ncols: 2,
+            nrows: 2,
+            data: vec![1.0, 2., 3., 4.],
+        };
+        let b: Matrix = Matrix {
+            ncols: 2,
+            nrows: 2,
+            data: vec![5., 6., 7., 8.],
+        };
+        let c: Matrix = a.multiply(&b);
+        let expected: Matrix = Matrix {
+            ncols: 2,
+            nrows: 2,
+            data: vec![19.0, 22.0, 43.0, 50.0],
+        };
+
+        assert_eq!(c.data, expected.data);
+        assert_eq!(c.nrows, expected.nrows);
+        assert_eq!(c.ncols, expected.ncols);
+    }
 }
