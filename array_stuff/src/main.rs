d4df15cdc68199136229259f651867f5b897bc20
thomasalcock <thomas.alcock@statworx.com>
2024-03-27 21:31:18 +0100

    add method for matrix addition

--- array_stuff/src/main.rs
+++ array_stuff/src/main.rs
@@ -16,8 +16,8 @@ impl Matrix {
             ncols: ncols,
             data: vec![0.0; nrows * ncols],
         };
-        for _ in 0..(nrows * ncols) {
-            matrix.data.push(rng.gen_range(min..max));
+        for i in 0..(nrows * ncols) {
+            matrix.data[i] = rng.gen_range(min..max);
         }
         return matrix;
     }
@@ -28,6 +28,10 @@ impl Matrix {
             let start: usize = i * self.ncols;
             println!("{:?}", &self.data[start..(start + self.ncols)]);
         }
+    }
+
+    fn dimensions(&self) {
+        println!("nrows={}, ncols={}", self.nrows, self.ncols);
     }
 
     fn multiply(&self, m: &Matrix) -> Matrix {
@@ -54,24 +58,48 @@ impl Matrix {
 
         return result;
     }
+
+    fn add(&self, m: &Matrix) -> Matrix {
+        let mut result: Matrix = Matrix {
+            nrows: self.nrows,
+            ncols: m.ncols,
+            data: vec![0.0; self.nrows * self.ncols],
+        };
+        let mut lhs: f32;
+        let mut rhs: f32;
+        for i in 0..self.nrows {
+            for j in 0..self.ncols {
+                rhs =  m.data[i * self.ncols + j];
+                lhs = self.data[i * self.ncols + j];
+                result.data[i * self.ncols + j] = lhs + rhs;
+            }
+        }
+        return result;
+    }
 }
 
 fn main() {
-    let a: Matrix = Matrix {
-        ncols: 2,
-        nrows: 2,
-        data: vec![1.0, 2., 3., 4.],
-    };
-    let b: Matrix = Matrix {
-        ncols: 2,
-        nrows: 2,
-        data: vec![5., 6., 7., 8.],
-    };
-    let c: Matrix = a.multiply(&b);
+    let features: Matrix = Matrix::random(10, 2, 10.0, 40.0);
+    let outputs: Matrix = Matrix::random(10, 1, 5.0, 6.0);
+    let mut weights: Matrix = Matrix::random(2, 1, 0.1, 1.0);
+    
+    let alpha: f32 = 0.01;
+    let n_epochs: usize = 5;
 
-    a.print_matrix();
-    b.print_matrix();
-    c.print_matrix();
+    for _  in 0..n_epochs {
+        let predictions: Matrix = features.multiply(&weights);
+        let mut error: f32 = 0.0;
+        let n_obs: usize  = outputs.data.len();
+
+        for i in 0..n_obs {
+            error += (predictions.data[i] - outputs.data[i]).powf(2.0) / n_obs as f32;
+        }
+        for i in 0..weights.nrows {
+            weights.data[i] -= alpha * error;
+        }
+        println!("error = {}", error);
+        weights.print_matrix();
+    }
 }
 
 #[cfg(test)]
@@ -99,5 +127,26 @@ mod tests {
         assert_eq!(c.data, expected.data);
         assert_eq!(c.nrows, expected.nrows);
         assert_eq!(c.ncols, expected.ncols);
+    }
+    #[test]
+    fn matrix_add_works() {
+        let a: Matrix = Matrix {
+            ncols: 2,
+            nrows: 2,
+            data: vec![1.0, 2., 3., 4.],
+        };
+        let b: Matrix = Matrix {
+            ncols: 2,
+            nrows: 2,
+            data: vec![5., 6., 7., 8.],
+        };
+        let c: Matrix = a.add(&b);
+        let expected: Matrix = Matrix {
+            ncols: 2,
+            nrows: 2,
+            data: vec![6.0, 8.0, 10.0, 12.0],
+        };
+        assert_eq!(c.data, expected.data);
+
     }
 }
